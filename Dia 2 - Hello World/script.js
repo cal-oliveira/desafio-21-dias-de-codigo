2736222196e0f4b05117ac8de08f353b13dbc89d
Calebe Oliveira <calebe81899026@gmail.com>
2022-08-16 22:35:39 -0300

    finished

--- Dia 2 - Hello World/script.js
+++ Dia 2 - Hello World/script.js
@@ -1,15 +1,18 @@
 let h1 = document.getElementById('h1')
 let body = document.getElementById('body')
 let btn = document.getElementById('btn')
+let flagIcon = document.getElementById('flag-icon')
 
 function change(){
 
    if(h1.textContent === 'Hello World!'){
     h1.innerText = 'Olá, mundo!'
+    flagIcon.setAttribute('src','./images/br.png')
     return
    }
 
    if(h1.textContent === 'Olá, mundo!'){
+    flagIcon.setAttribute('src','./images/us.png')
     h1.innerText = 'Hello World!'
    }
 
