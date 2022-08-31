b083661b3b57a576a3eb11fb7a92cf1f64b740a1
Calebe Oliveira <calebe81899026@gmail.com>
2022-08-31 13:09:46 -0300

    finished

--- Dia 14 - Formulario de quizzes/script.js
+++ Dia 14 - Formulario de quizzes/script.js
@@ -2,18 +2,29 @@ let option1 = document.getElementById('option1')
 let option2 = document.getElementById('option2')
 let option3 = document.getElementById('option3')
 
+let res = 3
+
 option1.addEventListener('click',()=>{
     option1.classList.add('selected')
 
-    setTimeout(()=>{
-
-    },3000)
+    option1.classList.add('wrong')
+    option2.classList.add('wrong')
+    option3.classList.add('correct')
+    
 })
 
 option2.addEventListener('click',()=>{
     option2.classList.add('selected')
+
+    option1.classList.add('wrong')
+    option2.classList.add('wrong')
+    option3.classList.add('correct')
 })
 
 option3.addEventListener('click',()=>{
     option3.classList.add('selected')
+
+    option1.classList.add('wrong')
+    option2.classList.add('wrong')
+    option3.classList.add('correct')
 })
