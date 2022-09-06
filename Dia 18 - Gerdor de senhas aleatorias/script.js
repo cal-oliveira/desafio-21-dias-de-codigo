e22979f151c15784836af103ed94c5fec4f4c14d
Calebe Oliveira <calebe81899026@gmail.com>
2022-09-06 18:21:00 -0300

    finished implementations

--- Dia 18 - Gerdor de senhas aleatorias/script.js
+++ Dia 18 - Gerdor de senhas aleatorias/script.js
@@ -1,3 +1,14 @@
-let pass = document.getElementById('pass')
+let passElement = document.getElementById('pass')
+let pass
 
-console.log(pass)
+function newPass(){
+    pass = Math.random().toString(36).substr(2)
+    passElement.innerText = pass
+}
+
+function copy(){
+    console.log(pass)
+    navigator.clipboard.writeText(pass)
+}
+
+newPass()
